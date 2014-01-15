81778ad6acd34c51eed0c5a71fe2cb20b8f6c83c
Johannes Hoppe <info@johanneshoppe.com>
2014-01-15 11:37:32 +0100

    adds: tests

--- tests/testproject/models.py
+++ tests/testproject/models.py
@@ -3,45 +3,43 @@ from stdimage import StdImageField
 
 
 class SimpleModel(models.Model):
-    # works as ImageField
+    """works as ImageField"""
     image = StdImageField(upload_to='img')
 
 
 class AdminDeleteModel(models.Model):
-    # can be deleted through admin
+    """can be deleted through admin"""
     image = StdImageField(upload_to='img', blank=True)
 
 
 class ResizeModel(models.Model):
-    # resizes image to maximum size to fit a 640x480 area
-    image = StdImageField(upload_to='img', size=(640, 480))
+    """resizes image to maximum size to fit a 640x480 area"""
+    image = StdImageField(upload_to='img', variations={'medium': (640, 480)})
 
 
 class ResizeCropModel(models.Model):
-    # resizes image to 640x480 croping if necessary
-    image = StdImageField(upload_to='img', size=(640, 480, True))
+    """resizes image to 640x480 cropping if necessary"""
+    image = StdImageField(upload_to='img', variations={'medium': (640, 480, True)})
 
 
 class ThumbnailModel(models.Model):
-    # creates a thumbnail resized to maximum size to fit a 100x75 area
-    image = StdImageField(upload_to='img', blank=True,
-                          thumbnail_size=(100, 75))
+    """creates a thumbnail resized to maximum size to fit a 100x75 area"""
+    image = StdImageField(upload_to='img', blank=True, variations={'thumbnail': (100, 75)})
 
 
 class ThumbnailCropModel(models.Model):
-    # creates a thumbnail resized to 100x100 croping if necessary
-    image = StdImageField(upload_to='img', thumbnail_size=(100, 100, True))
+    """creates a thumbnail resized to 100x100 croping if necessary"""
+    image = StdImageField(upload_to='img', variations={'thumbnail': (100, 100, True)})
 
 
 class MultipleFieldsModel(models.Model):
-    # creates a thumbnail resized to 100x100 croping if necessary
-    image1 = StdImageField(upload_to='img', thumbnail_size=(100, 100, True))
+    """creates a thumbnail resized to 100x100 croping if necessary"""
+    image1 = StdImageField(upload_to='img', variations={'thumbnail': (100, 100, True)})
     image2 = StdImageField(upload_to='img')
     image3 = StdImageField('Some label', upload_to='img')
     text = models.CharField('Some label', max_length=10)
 
 
 class AllModel(models.Model):
-    # all previous features in one declaration
-    image = StdImageField(upload_to='img', blank=True, variations={'size': (640, 480),
-                                                                   'thumbnail_size': (100, 100, True)})
+    """all previous features in one declaration"""
+    image = StdImageField(upload_to='img', blank=True, variations={'size': (640, 480), 'thumbnail': (100, 100, True)})
