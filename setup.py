66ed388d9694ea0c9d1ef515af01e8042d14ca39
Johannes Hoppe <info@johanneshoppe.com>
2014-03-29 13:15:52 +0100

    fixes #15

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-stdimage',
-    version='0.5.9',
+    version='0.5.10',
     description='Django Standarized Image Field',
     author='codingjoe',
     url='https://github.com/codingjoe/django-stdimage',
--- stdimage/__init__.py
+++ stdimage/__init__.py
@@ -4,17 +4,6 @@ from .fields import StdImageField
 
 try:
     from south.modelsinspector import add_introspection_rules
-    rules = [
-      (
-        (StdImageField,),
-        [],
-        {
-            "variations": ["variations", {"default": None}],
-            "min_size": ["min_size", {"default": None}],
-            "max_size": ["max_size", {"default": None}],
-        },
-      )
-    ]
-    add_introspection_rules(rules, ["^stdimage\.fields"])
+    add_introspection_rules([], ["^stdimage\.fields"])
 except ImportError:
     pass
