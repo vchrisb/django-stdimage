ec34e26b191ac7a3cf371ce103a64a8e812f1c7f
Johannes Hoppe <info@johanneshoppe.com>
2014-08-26 20:30:48 +0200

    hotfix - south rules where outdated

--- stdimage/models.py
+++ stdimage/models.py
@@ -177,8 +177,6 @@ try:
     add_introspection_rules(
         [([StdImageField], [], {
             "variations": ["variations", {"default": None}],
-            "min_size": ["min_size", {"default": None}],
-            "max_size": ["max_size", {"default": None}],
         },), ],
         ["^stdimage\.models\.StdImageField"]
     )
