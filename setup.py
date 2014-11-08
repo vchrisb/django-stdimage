16ad05c25490ab5dafffe5e31753d16d3c5c99f3
Johannes Hoppe <info@johanneshoppe.com>
2014-11-08 16:38:29 +0100

    Added fields.py for legacy issues
    Old south migrations might still need a path to the fields.py file.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class PyTest(Command):
 
 setup(
     name='django-stdimage',
-    version='1.1.0',
+    version='1.1.1',
     description='Django Standarized Image Field',
     author='codingjoe',
     url='https://github.com/codingjoe/django-stdimage',
