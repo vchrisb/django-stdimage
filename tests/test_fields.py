4a2aeaf5122646bcb7f172413c32c477df9a347d
Johannes Hoppe <info@johanneshoppe.com>
2015-06-01 11:05:22 +0100

    Removed deprecated fields.py

--- tests/test_fields.py
+++
@@ -1,9 +0,0 @@
-from stdimage import fields
-
-
-class TestStdImageField(object):
-
-    def test_deprecation_warning(self, recwarn):
-        fields.StdImageField()
-        w = recwarn.pop(DeprecationWarning)
-        assert issubclass(w.category, DeprecationWarning)
