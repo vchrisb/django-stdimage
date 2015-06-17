01163cafc5682ea1c9cfe70352a9c424c6cb9f96
Johannes Hoppe <info@johanneshoppe.com>
2015-06-17 16:53:56 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class PyTest(Command):
 
 setup(
     name='django-stdimage',
-    version='2.0.0',
+    version='2.0.1',
     description='Django Standarized Image Field',
     author='codingjoe',
     url='https://github.com/codingjoe/django-stdimage',
