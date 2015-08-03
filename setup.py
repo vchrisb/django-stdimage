53a43133979173df1049a4e1e6c87a87cb1b5e4c
Johannes Hoppe <info@johanneshoppe.com>
2015-08-03 09:49:33 +0200

    Bupled version number

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class PyTest(Command):
 
 setup(
     name='django-stdimage',
-    version='2.0.1',
+    version='2.0.2',
     description='Django Standarized Image Field',
     author='codingjoe',
     url='https://github.com/codingjoe/django-stdimage',
