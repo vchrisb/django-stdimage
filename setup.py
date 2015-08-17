2403197db413aae50642b3deda96efb8bdf2189b
Johannes Hoppe <info@johanneshoppe.com>
2015-08-17 23:57:52 +0200

    Fixed infinity float issue

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ if 'sdist' in sys.argv or 'develop' in sys.argv:
 
 setup(
     name='django-stdimage',
-    version='2.0.3',
+    version='2.0.4',
     description='Django Standarized Image Field',
     author='codingjoe',
     url='https://github.com/codingjoe/django-stdimage',
