2858311be9e0065bbfe984d74b106f1735c38311
Johannes Hoppe <info@johanneshoppe.com>
2017-01-05 16:14:03 +0100

    Release 2.4.0 -- Add progressive JPEGS

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-stdimage',
-    version='2.3.3',
+    version='2.4.0',
     description='Django Standarized Image Field',
     author='codingjoe',
     url='https://github.com/codingjoe/django-stdimage',
