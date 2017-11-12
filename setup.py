83a6f572d0ff0bdb01df1a2f40ffb7e669fd6b0f
Johannes Hoppe <info@johanneshoppe.com>
2017-11-12 11:09:31 +0100

    Switch to pbr for releases

--- setup.py
+++ setup.py
@@ -1,38 +1,7 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
-    name='django-stdimage',
-    version='2.4.2',
-    description='Django Standarized Image Field',
-    author='codingjoe',
-    url='https://github.com/codingjoe/django-stdimage',
-    download_url='https://github.com/codingjoe/django-stdimage',
-    author_email='info@johanneshoppe.com',
-    license='MIT',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
-    ],
-    packages=find_packages(exclude=[
-        "*.tests", "*.tests.*", "tests.*", "tests", ".egg-info"
-    ]),
-    include_package_data=True,
-    install_requires=[
-        'pillow>=2.5',
-        'progressbar2>=3.0.0',
-    ],
+    setup_requires=['pbr'],
+    pbr=True,
 )
