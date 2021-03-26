2606fd22fbcd6145bd686d22b5ee973749c9668c
Johannes Maron <info@johanneshoppe.com>
2021-03-26 09:29:09 +0100

    Update package and CI setup

--- setup.py
+++ setup.py
@@ -5,53 +5,54 @@ import os
 import subprocess  # nosec
 from distutils.cmd import Command
 from distutils.command.build import build as _build
+from distutils.command.install import install as _install
 
 from setuptools import setup
-from setuptools.command.install_lib import install_lib as _install_lib
 
 BASE_DIR = os.path.dirname((os.path.abspath(__file__)))
 
 
 class compile_translations(Command):
-    description = 'Compile i18n translations using gettext.'
+    description = "Compile i18n translations using gettext."
     user_options = []
 
     def initialize_options(self):
-        pass
+        self.build_lib = None
 
     def finalize_options(self):
-        pass
+        self.set_undefined_options("build", ("build_lib", "build_lib"))
 
     def run(self):
-        pattern = 'stdimage/locale/*/LC_MESSAGES/django.po'
+        pattern = "stdimage/locale/*/LC_MESSAGES/django.po"
         for file in glob.glob(pattern):
-            cmd = ['msgfmt', '-c']
             name, ext = os.path.splitext(file)
-
-            cmd += ['-o', '%s.mo' % name]
-            cmd += ['%s.po' % name]
+            cmd = ["msgfmt", "-c", "-o", f"{self.build_lib}/{name}.mo", file]
             self.announce(
-                'running command: %s' % ' '.join(cmd),
-                level=distutils.log.INFO)
+                "running command: %s" % " ".join(cmd), level=distutils.log.INFO
+            )
             subprocess.check_call(cmd, cwd=BASE_DIR)  # nosec
 
 
 class build(_build):
-    sub_commands = [('compile_translations', None)] + _build.sub_commands
+    sub_commands = [
+        *_build.sub_commands,
+        ("compile_translations", None),
+    ]
 
 
-class install_lib(_install_lib):
-    def run(self):
-        self.run_command('compile_translations')
-        _install_lib.run(self)
+class install(_install):
+    sub_commands = [
+        *_install.sub_commands,
+        ("compile_translations", None),
+    ]
 
 
 setup(
     name='django-stdimage',
     use_scm_version=True,
     cmdclass={
-        'build': build,
-        'install_lib': install_lib,
-        'compile_translations': compile_translations,
+        "build": build,
+        "install": install,
+        "compile_translations": compile_translations,
     },
 )
