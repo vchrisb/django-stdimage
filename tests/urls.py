8826c7b5fe2294ff7b406dd6a3be995aa55f4310
Johannes Maron <info@johanneshoppe.com>
2021-03-26 09:45:59 +0100

    Resolve deprecation errors and add Django 3.2 ci suite

--- tests/urls.py
+++ tests/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
